6df181324cf2bdeb631953a6457800db15f38e8d
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2025-09-12 14:55:33 +0800

    [dev] teacher profile

--- app/Livewire/Setting/Profile/TeacherProfile.php
+++ app/Livewire/Setting/Profile/TeacherProfile.php
@@ -2,10 +2,118 @@
 
 namespace App\Livewire\Setting\Profile;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Rule;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class TeacherProfile extends Component
 {
+    use WithFileUploads;
+
+    public $nama;
+    public $nip;
+    public $nuptk;
+    public $nomorPonsel;
+    public $jenisKelamin;
+    public $agama;
+    public $tempatLahir;
+    public $tanggalLahir;
+    public $alamat;
+    public $tanggalBergabung;
+    public $kodePos;
+
+    public $foto;
+    public $fotoUrl;
+
+    public function rules(){
+        return [
+            'nama'              => ['required','string','max:100'],
+            'nip'               => ['required','string','max:25'],
+            'nuptk'             => ['required','string','max:25'],
+            'nomorPonsel'       => ['nullable','string','max:25'],
+            'jenisKelamin'      => ['required', Rule::in(config('const.sex'))],
+            'agama'             => ['required', Rule::in(config('const.religions'))],
+            'tempatLahir'       => ['required','string','max:100'],
+            'tanggalLahir'      => ['required','date'],
+            'alamat'            => ['nullable','string','max:255'],
+            'tanggalBergabung'  => ['nullable','date'],
+            'kodePos'           => ['nullable','string','max:10'],
+            'foto'              => ['nullable','image','max:1024'],
+        ];
+    }
+
+    public function edit(){
+        $this->validate();
+
+        try {
+            DB::beginTransaction();
+
+            $teacher = auth()->user()->teacher;
+
+            $teacher->name              = $this->nama;
+            $teacher->nip               = $this->nip;
+            $teacher->nuptk             = $this->nuptk;
+            $teacher->phone             = $this->nomorPonsel;
+            $teacher->sex               = $this->jenisKelamin;
+            $teacher->religion          = $this->agama;
+            $teacher->place_of_birth    = $this->tempatLahir;
+            $teacher->birth_date        = $this->tanggalLahir;
+            $teacher->address           = $this->alamat;
+            $teacher->date_joined       = $this->tanggalBergabung;
+            $teacher->postal_code       = $this->kodePos;
+
+            if ($this->foto) {
+                $path = $this->foto->store('teacher-photos', 'public');
+                $teacher->photo = $path;
+            }
+
+            $teacher->save();
+
+            session()->flash('alert', [
+                'type' => 'success',
+                'message' => 'Berhasil!',
+                'detail' => "Berhasil menyunting data profil guru.",
+            ]);
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            logger()->error(
+                '[profil guru] ' .
+                    auth()->user()->username .
+                    ' gagal menyunting profil guru',
+                [$e->getMessage()]
+            );
+
+            session()->flash('alert', [
+                'type' => 'danger',
+                'message' => 'Gagal!',
+                'detail' => "Gagal menyunting data profil guru.",
+            ]);
+        }
+
+        return redirect()->back();
+    }
+
+    public function mount(){
+        $teacher = auth()->user()->teacher;
+
+        $this->nama             = $teacher->name;
+        $this->nip              = $teacher->nip;
+        $this->nuptk            = $teacher->nuptk;
+        $this->nomorPonsel      = $teacher->phone;
+        $this->jenisKelamin     = $teacher->sex;
+        $this->agama            = $teacher->religion;
+        $this->tempatLahir      = $teacher->place_of_birth;
+        $this->tanggalLahir     = $teacher->birth_date;
+        $this->alamat           = $teacher->address;
+        $this->tanggalBergabung = $teacher->date_joined;
+        $this->kodePos          = $teacher->postal_code;
+        $this->fotoUrl          = $teacher->photoUrl();
+    }
+
     public function render()
     {
         return view('livewire.setting.profile.teacher-profile');
--- resources/views/livewire/setting/profile/teacher-profile.blade.php
+++ resources/views/livewire/setting/profile/teacher-profile.blade.php
@@ -1,3 +1,90 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <x-alert />
+
+    <form class="card" wire:submit.prevent="edit" autocomplete="off">
+        <div class="card-header">Sunting Profile Guru</div>
+
+        <div class="card-body">
+            <div class="row">
+                <div class="col-lg-6 col-12">
+                    <x-form.input required wire:model="nama" name="nama" label="Nama" type="text"
+                        placeholder="masukkan nama" autofocus />
+
+                    <x-form.input required wire:model="nip" name="nip" label="NIP" type="text"
+                        placeholder="masukkan nomor induk pegawai (nip)" />
+
+                    <x-form.input required wire:model="nuptk" name="nuptk" label="NUPTK" type="text"
+                        placeholder="masukkan nomor unik pendidik & tenaga kependidikan (nuptk)" />
+
+                    <x-form.input required wire:model="nomorPonsel" name="nomorPonsel" label="Nomor Ponsel"
+                        type="text" placeholder="masukkan nomor ponsel" />
+
+                    <x-form.select required wire:model="agama" name="agama" label="Agama">
+                        <option value="" selected>- Pilih Agama -</option>
+                        @foreach (config('const.religions') as $agama)
+                            <option value="{{ $agama }}">{{ $agama }}</option>
+                        @endforeach
+                    </x-form.select>
+
+                    <div class="mb-3">
+                        <label class="form-label" for="jenisKelamin">Jenis Kelamin <span
+                                class="ms-1 text-red">*</span></label>
+
+                        <div class="d-flex flex-row gap-3">
+                            @foreach (config('const.sex') as $jenisKelamin)
+                                <x-form.check required wire:model="jenisKelamin" name="jenisKelamin" type="radio"
+                                    value="{{ $jenisKelamin }}" description="{{ $jenisKelamin }}" />
+                            @endforeach
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div
+                            class="col-lg-2 align-self-center text-lg-start text-center mt-lg-0 mt-4 mb-lg-0 mb-2 mb-lg-3">
+                            @if ($foto)
+                                <span class="avatar avatar-md"
+                                    style="background-image: url({{ $foto->temporaryUrl() }})"></span>
+                            @else
+                                <span class="avatar avatar-md"
+                                    style="background-image: url({{ $fotoUrl }})"></span>
+                            @endif
+                        </div>
+
+                        <div class="col-lg-9 col-auto mt-3 mt-lg-0 ms-lg-3">
+                            <x-form.input wire:model.lazy="foto" name="foto" label="Foto Anda (Latar Merah)"
+                                type="file" optional="Abaikan jika tidak ingin mengubah." />
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-12">
+                    <x-form.input wire:model="tanggalLahir" name="tanggalLahir" label="Tanggal Lahir" type="date"
+                        required />
+
+                    <x-form.input wire:model="tempatLahir" name="tempatLahir" label="Tempat Lahir" type="text"
+                        placeholder="masukkan tempat lahir" required />
+
+                    <x-form.textarea wire:model="alamat" name="alamat" label="Alamat" placeholder="masukkan alamat" />
+
+                    <div class="row">
+                        <div class="col-lg-6 col-12">
+                            <x-form.input wire:model="kodePos" name="kodePos" label="Kode Pos" type="text" />
+                        </div>
+                        <div class="col-lg-6 col-12">
+                            <x-form.input wire:model="tanggalBergabung" name="tanggalBergabung"
+                                label="Tanggal Bergabung" type="date" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card-footer">
+            <div class="btn-list justify-content-end">
+                <button type="reset" class="btn">Reset</button>
+
+                <x-datatable.button.save name="Simpan Perubahan" target="edit" />
+            </div>
+        </div>
+    </form>
 </div>
