d9e53e972f0b0863d385b61c314f27e0f8f3e77c
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2025-11-16 16:04:51 +0800

    fix: developer

--- routes/web.php
+++ routes/web.php
@@ -47,7 +47,7 @@ Route::middleware('auth', 'verified', 'force.logout')->namespace('App\Livewire')
         /**
          * class schedule / jadwal kelas
          */
-        Route::prefix('jadwal-kelas')->name('class-schedule.')->middleware('roles:admin')->group(function () {
+        Route::prefix('jadwal-kelas')->name('class-schedule.')->middleware('roles:admin,developer')->group(function () {
             Route::get('/', ClassSchedule\Index::class)->name('index');
             Route::get('/tambah', ClassSchedule\Create::class)->name('create');
             Route::get('/sunting/{id}', ClassSchedule\Edit::class)->name('edit');
@@ -56,7 +56,7 @@ Route::middleware('auth', 'verified', 'force.logout')->namespace('App\Livewire')
         /**
          * admin
          */
-        Route::prefix('admin')->name('admin.')->middleware('roles:admin')->group(function () {
+        Route::prefix('admin')->name('admin.')->middleware('roles:admin,developer')->group(function () {
             Route::get('/', Admin\Index::class)->name('index');
             Route::get('/tambah', Admin\Create::class)->name('create');
             Route::get('/{id}/edit', Admin\Edit::class)->name('edit');
@@ -65,21 +65,21 @@ Route::middleware('auth', 'verified', 'force.logout')->namespace('App\Livewire')
         /**
          * class room / ruang kelas
          */
-        Route::prefix('ruang-kelas')->name('classroom.')->middleware('roles:admin')->group(function () {
+        Route::prefix('ruang-kelas')->name('classroom.')->middleware('roles:admin,developer')->group(function () {
             Route::get('/', ClassRoom\Index::class)->name('index');
         });
 
         /**
          * subject study / mata pelajaran
          */
-        Route::prefix('mata-pelajaran')->name('subject-study.')->middleware('roles:admin')->group(function () {
+        Route::prefix('mata-pelajaran')->name('subject-study.')->middleware('roles:admin,developer')->group(function () {
             Route::get('/', SubjectStudy\Index::class)->name('index');
         });
 
         /**
          *  class advisor / wali kelas
          */
-        Route::prefix('wali-kelas')->name('advisor-class.')->middleware('roles:admin')->group(function () {
+        Route::prefix('wali-kelas')->name('advisor-class.')->middleware('roles:admin,developer')->group(function () {
             Route::get('/', ClassAdvisor\Index::class)->name('index');
         });
     });
