df1e0fc842a7c5478ace77b3cbc58d5ad0b6962b
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2025-11-26 22:27:09 +0800

    add: import export student guardian view

--- app/Livewire/StudentGuardian/Index.php
+++ app/Livewire/StudentGuardian/Index.php
@@ -2,14 +2,19 @@
 
 namespace App\Livewire\StudentGuardian;
 
+use App\Exports\StudentGuardianExport;
+use App\Imports\StudentGuardianImport;
 use App\Livewire\Traits\DataTable\WithBulkActions;
 use App\Livewire\Traits\DataTable\WithCachedRows;
 use App\Livewire\Traits\DataTable\WithPerPagePagination;
 use App\Livewire\Traits\DataTable\WithSorting;
 use App\Models\StudentGuardian;
+use Exception;
+use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Computed;
 use Livewire\Attributes\On;
 use Livewire\Component;
+use Maatwebsite\Excel\Facades\Excel;
 
 class Index extends Component
 {
@@ -23,6 +28,98 @@ class Index extends Component
         'nis_student' => '',
         'student_name' => '',
     ];
+
+    public $showModalExcel = false;
+    public $fileExcel;
+
+    public function closeModalExcel()
+    {
+        $this->showModalExcel = false;
+    }
+
+    public function openModalExcel()
+    {
+        $this->showModalExcel = true;
+    }
+
+    public function exportExcel()
+    {
+        try {
+            return Excel::download(new StudentGuardianExport, 'data-wali-siswa.xlsx');
+        } catch (Exception $e) {
+
+            logger()->error(
+                '[export excel student] ' .
+                    auth()->user()->username .
+                    ' gagal export data wali siswa',
+                [$e->getMessage()]
+            );
+
+            session()->flash('alert', [
+                'type' => 'danger',
+                'message' => 'Gagal!',
+                'detail' => 'Export data wali siswa gagal dilakukan.',
+            ]);
+
+            $this->resetForm();
+            return redirect()->back();
+        }
+
+        session()->flash('alert', [
+            'type' => 'success',
+            'message' => 'Berhasil!',
+            'detail' => 'Export data wali siswa berhasil dilakukan.',
+        ]);
+
+        $this->resetForm();
+        return redirect()->back();
+    }
+
+    public function importExcel()
+    {
+        try {
+            DB::beginTransaction();
+
+            Excel::queueImport(new StudentGuardianImport, $this->fileExcel);
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::beginTransaction();
+
+            logger()->error(
+                '[import excel student guardian] ' .
+                    auth()->user()->username .
+                    ' gagal import data wali siswa',
+                [$e->getMessage()]
+            );
+
+            session()->flash('alert', [
+                'type' => 'danger',
+                'message' => 'Gagal.',
+                'detail' => "import data wali siswa gagal dilakukan.",
+            ]);
+
+            $this->resetForm();
+            return redirect()->back();
+        }
+
+        session()->flash('alert', [
+            'type' => 'success',
+            'message' => 'Berhasil.',
+            'detail' => "import data wali siswa berhasil dilakukan.",
+        ]);
+
+        $this->resetForm();
+        return redirect()->back();
+    }
+
+    public function resetForm()
+    {
+        $this->reset([
+            'showModalExcel',
+            'fileExcel',
+        ]);
+    }
 
     public function deleteSelected()
     {
