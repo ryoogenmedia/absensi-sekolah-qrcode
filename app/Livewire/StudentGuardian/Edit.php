9c07c2b2a10463a6f1571076f367b657ae56e389
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2025-11-26 18:22:12 +0800

    add: scheme livewire student guardian

---
+++ app/Livewire/StudentGuardian/Edit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\StudentGuardian;
+
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public function render()
+    {
+        return view('livewire.student-guardian.edit');
+    }
+}
