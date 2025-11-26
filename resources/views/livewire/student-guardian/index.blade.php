006cd0c3a5846ec50bdbbb787aa44585095a58cb
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2025-11-26 19:26:10 +0800

    add: guardian index view

--- resources/views/livewire/student-guardian/index.blade.php
+++ resources/views/livewire/student-guardian/index.blade.php
@@ -1,3 +1,136 @@
 <div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+    <x-slot name="title">Wali Siswa</x-slot>
+
+    <x-slot name="pageTitle">Wali Siswa</x-slot>
+
+    <x-slot name="pagePretitle">Kelola Data Wali Siswa</x-slot>
+
+    <x-slot name="button">
+        <x-datatable.button.add name="Tambah Wali Siswa" :route="route('student-guardian.create')" />
+    </x-slot>
+
+    <x-alert />
+
+    <x-modal.delete-confirmation />
+
+    <div class="row mb-3 align-items-center justify-content-between">
+        <div class="col-12 col-lg-8 d-flex align-self-center">
+            <div>
+                <x-datatable.search placeholder="Cari nama wali siswa / siswa..." />
+            </div>
+        </div>
+        <div class="col-auto ms-auto d-flex mt-lg-0 mt-3">
+            <x-datatable.items-per-page />
+
+            <x-datatable.bulk.dropdown>
+                <div class="dropdown-menu dropdown-menu-end datatable-dropdown">
+                    <button data-bs-toggle="modal" data-bs-target="#delete-confirmation" class="dropdown-item"
+                        type="button">
+                        <i class="las la-trash me-3"></i>
+
+                        <span>Hapus</span>
+                    </button>
+                </div>
+            </x-datatable.bulk.dropdown>
+
+            <button wire:click="muatUlang" class="btn py-1 ms-2"><span class="las la-redo-alt fs-1"></span></button>
+        </div>
+    </div>
+
+    <div wire:poll.30000ms class="card" wire:loading.class.delay="card-loading" wire:offline.class="card-loading">
+        <div class="table-responsive mb-0">
+            <table class="table card-table table-bordered datatable">
+                <thead>
+                    <tr>
+                        <th class="w-1">
+                            <x-datatable.bulk.check wire:model.lazy="selectPage" />
+                        </th>
+
+                        <th>
+                            <x-datatable.column-sort name="Wali Siswa" wire:click="sortBy('guardian_name')"
+                                :direction="$sorts['guardian_name'] ?? null" />
+                        </th>
+
+                        <th>
+                            <x-datatable.column-sort name="Nama Siswa" wire:click="sortBy('student.name')"
+                                :direction="$sorts['student.name'] ?? null" />
+                        </th>
+
+                        <th>
+                            <x-datatable.column-sort name="Hubungan" wire:click="sortBy('guardian_relationship')"
+                                :direction="$sorts['guardian_relationship'] ?? null" />
+                        </th>
+
+                        <th>
+                            <x-datatable.column-sort name="Nomor Ponsel" wire:click="sortBy('guardian_contact')"
+                                :direction="$sorts['guardian_contact'] ?? null" />
+                        </th>
+
+                        <th style="width: 10px"></th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @if ($selectPage)
+                        <tr>
+                            <td colspan="10" class="bg-orange-lt rounded-0">
+                                @if (!$selectAll)
+                                    <div class="text-orange">
+                                        <span>Anda telah memilih <strong>{{ $this->rows->total() }}</strong> wali siswa,
+                                            apakah
+                                            Anda mau memilih semua <strong>{{ $this->rows->total() }}</strong>
+                                            wali siswa?</span>
+
+                                        <button wire:click="selectedAll" class="btn btn-sm ms-2">
+                                            Pilih Semua Data Wali Siswa
+                                        </button>
+                                    </div>
+                                @else
+                                    <span class="text-pink">Anda sekarang memilih semua
+                                        <strong>{{ count($this->selected) }}</strong> wali siswa.
+                                    </span>
+                                @endif
+                            </td>
+                        </tr>
+                    @endif
+
+                    @forelse ($this->rows as $row)
+                        <tr wire:key="row-{{ $row->id }}">
+                            <td>
+                                <x-datatable.bulk.check wire:model.lazy="selected" value="{{ $row->id }}" />
+                            </td>
+
+                            <td>
+                                {{ $row->guardian_name ?? '-' }}
+                            </td>
+
+                            <td>{{ $row->student->full_name ?? '-' }}</td>
+
+                            <td>
+                                {{ $row->guardian_relationship ?? '-' }}
+                            </td>
+
+                            <td>
+                                {{ $row->guardian_contact ?? '-' }}
+                            </td>
+
+                            <td>
+                                <div class="d-flex">
+                                    <div class="ms-auto">
+                                        <a class="btn btn-sm" href="{{ route('student-guardian.edit', $row->id) }}">
+                                            Sunting
+                                        </a>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    @empty
+                        <x-datatable.empty colspan="10" />
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+
+        {{ $this->rows->links() }}
+    </div>
 </div>
