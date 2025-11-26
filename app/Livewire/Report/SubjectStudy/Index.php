f859e24a20a7c79481ce97bfc8f049cfc7e80778
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2025-11-27 01:18:25 +0800

    fix: report subject study view

--- app/Livewire/Report/SubjectStudy/Index.php
+++ app/Livewire/Report/SubjectStudy/Index.php
@@ -2,10 +2,53 @@
 
 namespace App\Livewire\Report\SubjectStudy;
 
+use App\Livewire\Traits\DataTable\WithBulkActions;
+use App\Livewire\Traits\DataTable\WithCachedRows;
+use App\Livewire\Traits\DataTable\WithPerPagePagination;
+use App\Livewire\Traits\DataTable\WithSorting;
+use App\Models\SubjectStudy;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Index extends Component
 {
+    use WithBulkActions;
+    use WithPerPagePagination;
+    use WithCachedRows;
+    use WithSorting;
+
+    public $filters = [
+        'search' => '',
+    ];
+
+    #[Computed()]
+    public function rows()
+    {
+        $query = SubjectStudy::query()
+            ->when(!$this->sorts, fn($query) => $query->first())
+            ->when($this->filters['search'], function ($query, $search) {
+                $query->where('name_subject', 'LIKE', "%$search%");
+            })->latest();
+
+        return $this->applyPagination($query);
+    }
+
+    #[Computed()]
+    public function allData()
+    {
+        return SubjectStudy::all();
+    }
+
+    public function updatedFilters()
+    {
+        $this->resetPage();
+    }
+
+    public function resetFilters()
+    {
+        $this->reset('filters');
+    }
+
     public function render()
     {
         return view('livewire.report.subject-study.index');
--- resources/views/livewire/report/subject-study/index.blade.php
+++ resources/views/livewire/report/subject-study/index.blade.php
@@ -1,3 +1,85 @@
+@push('styles')
+    <style>
+        .custom-header {
+            margin-bottom: 20px !important;
+            margin-top: -68px;
+        }
+    </style>
+@endpush
+
 <div>
-    {{-- Be like water. --}}
+    <x-slot name="title">Laporan Mata Pelajaran</x-slot>
+
+    <div class="row g-2 align-items-center mb-4">
+        <div class="col">
+            <div class="page-pretitle">
+                Cetak Laporan Mata Pelajaran
+            </div>
+            <h2 class="page-title">
+                Laporan Mata Pelajaran
+            </h2>
+        </div>
+
+        <div class="col-auto ms-auto d-print-none">
+            <div class="btn-list">
+                <a href="{{ route('print-report.subject-study') }}" target="_blank" class="btn btn-danger"
+                    class="btn btn-danger"><span class="las la-print fs-1 me-2"></span>Cetak
+                    Laporan Mapel</a>
+            </div>
+        </div>
+    </div>
+
+    <x-alert />
+
+    <div class="row mb-3 align-items-center justify-content-between">
+        <div class="col-12 col-lg-5 d-flex">
+            <div class="w-50">
+                <x-datatable.search placeholder="Cari nama mata pelajaran..." />
+            </div>
+        </div>
+        <div class="col-auto ms-auto d-flex mt-lg-0 mt-3">
+            <x-datatable.items-per-page />
+        </div>
+    </div>
+
+    <div class="card" wire:loading.class.delay="card-loading" wire:offline.class="card-loading">
+        <div class="table-responsive mb-0">
+            <table class="table card-table table-bordered datatable">
+                <thead>
+                    <tr>
+                        <th style="width: 200px">
+                            <x-datatable.column-sort name="Nama Mata Pelajaran" wire:click="sortBy('name_subject')"
+                                :direction="$sorts['name_subject'] ?? null" />
+                        </th>
+
+                        <th>
+                            <x-datatable.column-sort name="Description" wire:click="sortBy('description')"
+                                :direction="$sorts['description'] ?? null" />
+                        </th>
+
+                        <th class="text-center" style="width: 100px">
+                            <x-datatable.column-sort name="Status" wire:click="sortBy('status_active')"
+                                :direction="$sorts['status_active'] ?? null" />
+                        </th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @forelse ($this->rows as $row)
+                        <tr wire:key="row-{{ $row->id }}">
+                            <td><b>{{ $row->name_subject ?? '-' }}</b></td>
+
+                            <td>{{ $row->description ?? '-' }}</td>
+
+                            <td class="text-center">{{ $row->status_active ? 'Aktif' : 'Tidak Aktif' }}</td>
+                        </tr>
+                    @empty
+                        <x-datatable.empty colspan="10" />
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+
+        {{ $this->rows->links() }}
+    </div>
 </div>
