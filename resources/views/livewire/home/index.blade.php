ce553b45471e1522c47c32c87782c459780eb3c3
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2025-12-03 17:25:58 +0800

    little change

--- resources/views/livewire/home/index.blade.php
+++ resources/views/livewire/home/index.blade.php
@@ -38,7 +38,7 @@
 
                                 <div class="ms-2">
                                     <div class="header font-weight-bold">
-                                        @if (isset($login->student) && auth()->user()->role == 'siswa')
+                                        @if (isset($login->student))
                                             <small><b>{{ $login->student->full_name ?? '-' }}</b></small>
                                         @else
                                             <small><b>{{ $login->username ?? '-' }}</b></small>
