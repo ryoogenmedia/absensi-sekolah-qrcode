a342fa9e6d4a478c3b6381561e6b08363badc09e
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2026-03-29 13:29:53 +0800

    little change

--- resources/views/livewire/report/attendance/class/index.blade.php
+++ resources/views/livewire/report/attendance/class/index.blade.php
@@ -24,6 +24,8 @@
         </div>
     </div>
 
+
+
     <div class="row mb-1 align-items-center justify-content-between">
         <div class="col-12 col-lg-8 d-flex">
             <div class="w-100">
@@ -165,6 +167,21 @@
         {{-- Footer hanya tampil di tab list --}}
         <div x-show="currentTab == 'list'">
             {{ $this->rows->links() }}
+        </div>
+    </div>
+
+    <div style="z-index: 9999; position: fixed; bottom: 20px; right: 20px;"
+        class="d-flex flex-column align-items-end gap-2">
+
+        <div class="btn btn-blue shadow-lg" wire:loading.delay wire:target="tab, filters, search">
+            <div class="d-flex align-items-center gap-2">
+                <div class="spinner-border spinner-border-sm" role="status"></div>
+                <span>Memproses Data...</span>
+            </div>
+        </div>
+
+        <div class="btn btn-danger shadow-lg" wire:offline>
+            <i class="las la-wifi-slash me-2"></i> Anda sedang offline.
         </div>
     </div>
 
